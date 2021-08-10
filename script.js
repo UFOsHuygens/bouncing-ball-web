371443e88e5d9929b494fee1120ae50f066d3346
Felipe Vieira <87919419+UFOsHuygens@users.noreply.github.com>
2021-08-10 13:31:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var ball = document.querySelector("#ball");
+var obs = document.querySelector("#obstacle");
 var pos_x = 0
 var pos_y = 0
 var velocidade_x = 3
@@ -24,5 +25,27 @@ function movement() { // função que movimenta o objeto
     if (pos_y <= 0) { // colisão da bola com o chão
         velocidade_y = 4
     }
+
+    // colisão com o obstaculo
+
+    if (ball.getBoundingClientRect().left < obs.getBoundingClientRect().left+obs.clientWidth &&
+        ball.getBoundingClientRect().left+ball.clientWidth > obs.getBoundingClientRect().left &&
+        ball.getBoundingClientRect().top < obs.getBoundingClientRect().top+obs.clientHeight &&
+        ball.getBoundingClientRect().top+ball.clientHeight > obs.getBoundingClientRect().top) {
+            if (ball.getBoundingClientRect().left >= (obs.getBoundingClientRect().left-ball.clientWidth)) {
+                velocidade_x = -3
+            }
+            if (ball.getBoundingClientRect().left >= (obs.getBoundingClientRect().left+obs.clientWidth-ball.clientWidth)) {
+                velocidade_x = 3
+            }
+            if (ball.getBoundingClientRect().top >= obs.getBoundingClientRect().top-ball.clientHeight) {
+                velocidade_y = -3
+            }
+            if (ball.getBoundingClientRect().top >= obs.getBoundingClientRect().top+obs.clientHeight-ball.clientHeight) {
+                velocidade_y = 3
+            }
+    }
 }
+
+
 window.setInterval(movement, 1); // chamada da função que movimenta o objeto
